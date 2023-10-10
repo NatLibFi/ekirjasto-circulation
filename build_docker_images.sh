02918a215679592d4a78b950453b4ff89c6f86f4
Esa Holmberg <esa.holmberg@lingsoft.fi>
2023-10-10 17:53:21 +0300

    updated build script

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -4,7 +4,7 @@
 
 git clone --branch=main ssh://git.lingsoft.fi/home/git/palace_circulation_admin_ui.git
 
-docker compose build $* --progress=plain
+docker compose --progress=plain build $* 
 
 [ -d palace_circulation_admin_ui ] && rm -rf palace_circulation_admin_ui
 
