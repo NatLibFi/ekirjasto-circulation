329f04fbffbc52df53fef1f91bbbbfd39427f625
Esa Holmberg <esa.holmberg@lingsoft.fi>
2023-11-07 12:31:32 +0200

    test moved from build script to Dockerfile.admin

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -4,15 +4,15 @@ if [ "$1" = "admin" ]
 then
   [ -d palace_circulation_admin_ui ] && rm -rf palace_circulation_admin_ui
   git clone --branch=main ssh://git.lingsoft.fi/home/git/palace_circulation_admin_ui.git
-  cd palace_circulation_admin_ui
-  npm i
-  npm run test-finland
-  retval=$?
-  if [ $retval -ne 0 ]; then
-    echo "Admin UI test failed"
-	exit $retval
-  fi
-  cd ..
+#  cd palace_circulation_admin_ui
+#  npm i
+#  npm run test-finland
+#  retval=$?
+#  if [ $retval -ne 0 ]; then
+#    echo "Admin UI test failed"
+#	exit $retval
+#  fi
+#  cd ..
   docker compose --progress=plain build admin
   [ -d palace_circulation_admin_ui ] && rm -rf palace_circulation_admin_ui
   docker create --name temp-admin "${PWD##*/}"-admin:latest
