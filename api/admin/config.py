d0b9c0161b9fe07616458381c819001ba12fc4a4
ttuovinen <teemu.t.tuovinen@gmail.com>
2024-02-21 13:45:10 +0200

    Upgrade ekirjasto-circulation-admin to v0.1.0

--- api/admin/config.py
+++ api/admin/config.py
@@ -16,7 +16,7 @@ class OperationalMode(str, Enum):
 class Configuration(LoggerMixin):
     APP_NAME = "E-kirjasto Collection Manager"
     PACKAGE_NAME = "@natlibfi/ekirjasto-circulation-admin"
-    PACKAGE_VERSION = "0.0.1-post.8"
+    PACKAGE_VERSION = "0.1.0"
 
     STATIC_ASSETS = {
         "admin_js": "circulation-admin.js",
