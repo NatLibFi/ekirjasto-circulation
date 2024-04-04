bfba477b750ee5d9d540cd7dde1f8ad610c47471
Johannes Ylönen <johannes.ylonen@indium.fi>
2024-04-04 21:57:23 +0000

    Fix test

--- tests/api/finland/test_ekirjasto.py
+++ tests/api/finland/test_ekirjasto.py
@@ -250,14 +250,14 @@ class TestEkirjastoAuthentication:
             )
 
             assert (
-                doc["links"][6]["rel"] == "passkey_register_start"
-                and doc["links"][6]["href"]
+                doc["links"][7]["rel"] == "passkey_register_start"
+                and doc["links"][7]["href"]
                 == "http://localhost/test-library/ekirjasto/passkey/register/start?provider=E-kirjasto+provider+for+circulation+manager"
             )
 
             assert (
-                doc["links"][7]["rel"] == "passkey_register_finish"
-                and doc["links"][7]["href"]
+                doc["links"][8]["rel"] == "passkey_register_finish"
+                and doc["links"][8]["href"]
                 == "http://localhost/test-library/ekirjasto/passkey/register/finish?provider=E-kirjasto+provider+for+circulation+manager"
             )
 
