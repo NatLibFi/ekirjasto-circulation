324ea6a0c626012d1e145c45174ca0ed2563b558
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2024-07-18 11:14:36 +0300

    EKIR-209 Add timeout argument bigger than the default

--- api/odl.py
+++ api/odl.py
@@ -253,8 +253,8 @@ class BaseODLAPI(PatronActivityCirculationAPI[SettingsType, LibrarySettingsType]
         auth_header = "Basic %s" % base64.b64encode(f"{username}:{password}")
         headers["Authorization"] = auth_header
 
-        return HTTP.get_with_timeout(url, headers=headers)
-
+        return HTTP.get_with_timeout(url, headers=headers, timeout=30) # Added a longer timeout for loan checkouts
+    
     def _url_for(self, *args: Any, **kwargs: Any) -> str:
         """Wrapper around flask's url_for to be overridden for tests."""
         return url_for(*args, **kwargs)
