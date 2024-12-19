d5a89e682b9a5372b1809fab44f47ecfbda9f418
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2024-12-19 11:56:18 +0200

    EKIRJASTO-131 Remove date type

--- core/feed/types.py
+++ core/feed/types.py
@@ -163,7 +163,7 @@ class WorkEntryData(BaseModel):
     subtitle: FeedEntryType | None = None
     series: FeedEntryType | None = None
     imprint: FeedEntryType | None = None
-    selected: datetime | date | None = None
+    selected: datetime | None = None
 
     authors: list[Author] = field(default_factory=list)
     contributors: list[Author] = field(default_factory=list)
