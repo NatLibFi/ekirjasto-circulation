f51f5e4ab73b98fd4d54ae6862db6fa1f6354538
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2025-07-02 14:00:16 +0300

    EKIR-437 Handle trailing slash

---
+++ tests/core/util/test_pydantic.py
@@ -0,0 +1,15 @@
+import pytest
+from pydantic import TypeAdapter
+
+from palace.manager.util.pydantic import HttpUrl, RedisDsn
+
+
+class TestStrUrlTypes:
+    def test_http_url(self) -> None:
+        ta = TypeAdapter(HttpUrl)
+        validate = ta.validate_python
+
+        assert validate("http://localhost:6379") == "http://localhost:6379"
+        assert validate("http://localhost:6379/") == "http://localhost:6379"
+        assert validate("http://10.0.0.1/foo") == "http://10.0.0.1/foo"
+        assert validate("http://10.0.0.1/foo/") == "http://10.0.0.1/foo"
