6bdf701289a97125f3dbf40b98f8ca4dd26c0ec2
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2026-01-27 15:23:18 +0200

    EKIR-710 Remove slash

--- api/ekirjasto_authentication.py
+++ api/ekirjasto_authentication.py
@@ -54,7 +54,7 @@ from core.util.problem_detail import ProblemDetail
 
 class EkirjastoEnvironment(Enum):
     FAKE = "http://localhost"
-    DEVELOPMENT = "https://tunnistus-dev.e-kirjasto.fi/"
+    DEVELOPMENT = "https://tunnistus-dev.e-kirjasto.fi"
     PRODUCTION = "https://tunnistus.e-kirjasto.fi"
     OLD_DEV = "https://e-kirjasto.loikka.dev"
 
