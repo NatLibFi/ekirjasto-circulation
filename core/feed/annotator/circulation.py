61acf9ea749236dc1b67872d7a6e08129afcba47
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2026-01-29 10:33:46 +0200

    EKIR-529 Don't localize group titles

--- core/feed/annotator/circulation.py
+++ core/feed/annotator/circulation.py
@@ -9,7 +9,6 @@ import urllib.request
 from collections import defaultdict
 from typing import Any
 
-import default_lane_names_to_localize
 from dependency_injector.wiring import Provide, inject
 from flask import url_for
 from flask_babel import lazy_gettext as _
@@ -959,14 +958,6 @@ class LibraryAnnotator(CirculationManagerAnnotator):
             entry.work, entry.license_pool, entry.identifier
         )
         if group_uri:
-            # Finland
-            # Attempt to translate the group title if it is to be localized.
-            if (
-                group_title in default_lane_names_to_localize.lanes.keys()
-                or group_title in localized_genre_names.keys()
-            ):
-                group_title = _(group_title)
-
             entry.computed.other_links.append(
                 Link(href=group_uri, rel=OPDSFeed.GROUP_REL, title=str(group_title))
             )
