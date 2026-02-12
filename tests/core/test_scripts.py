c138ca91db3b73299716407d3718e6c5bd271c40
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2026-02-12 13:07:14 +0200

    EKIR-734 Move other notification tests to same place

--- tests/core/test_scripts.py
+++ tests/core/test_scripts.py
@@ -4,7 +4,7 @@ import datetime
 import json
 import random
 from io import StringIO
-from unittest.mock import MagicMock, call, patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 from freezegun import freeze_time
@@ -13,7 +13,6 @@ from sqlalchemy.orm import Session
 
 from api.lanes import create_default_lanes
 from core.classifier import SubjectClassifier
-from core.config import Configuration, ConfigurationConstants
 from core.external_search import ExternalSearchIndex, Filter
 from core.lane import Lane, WorkList
 from core.metadata_layer import TimestampData
@@ -31,12 +30,9 @@ from core.model import (
     Work,
     WorkCoverageRecord,
     get_one,
-    get_one_or_create,
 )
 from core.model.classification import Classification, Subject
 from core.model.customlist import CustomList
-from core.model.devicetokens import DeviceToken, DeviceTokenTypes
-from core.model.patron import Patron
 from core.monitor import CollectionMonitor, Monitor, ReaperMonitor
 from core.opds_import import OPDSImportMonitor
 from core.scripts import (
@@ -55,7 +51,6 @@ from core.scripts import (
     IdentifierInputScript,
     LaneSweeperScript,
     LibraryInputScript,
-    LoanNotificationsScript,
     MockStdin,
     OPDSImportScript,
     PatronInputScript,
@@ -82,7 +77,6 @@ from core.scripts import (
     WorkProcessingScript,
 )
 from core.util.datetime_helpers import datetime_utc, utc_now
-from core.util.notifications import PushNotifications
 from core.util.worker_pools import DatabasePool
 from tests.core.mock import (
     AlwaysSuccessfulCollectionCoverageProvider,
@@ -2389,104 +2383,6 @@ class TestCustomListUpdateEntriesScript:
             session.refresh(prev_entry)
         assert custom_list.auto_update_status == CustomList.UPDATED
         assert custom_list.size == len(data.populated_books)
-
-
-class TestLoanNotificationsScript:
-    def _setup_method(self, db: DatabaseTransactionFixture):
-        self.script = LoanNotificationsScript(_db=db.session)
-        self.patron: Patron = db.patron()
-        self.work: Work = db.work(with_license_pool=True)
-        self.device_token, _ = get_one_or_create(
-            db.session,
-            DeviceToken,
-            patron=self.patron,
-            token_type=DeviceTokenTypes.FCM_ANDROID,
-            device_token="atesttoken",
-        )
-        PushNotifications.TESTING_MODE = True
-
-    def test_loan_notification(self, db: DatabaseTransactionFixture):
-        self._setup_method(db)
-        p = self.work.active_license_pool()
-        if p:  # mypy complains if we don't do this
-            loan, _ = p.loan_to(
-                self.patron,
-                utc_now(),
-                utc_now() + datetime.timedelta(days=1, hours=1),
-            )
-
-        work2 = db.work(with_license_pool=True)
-        loan2, _ = work2.active_license_pool().loan_to(
-            self.patron,
-            utc_now(),
-            utc_now() + datetime.timedelta(days=2, hours=1),
-        )  # Should not get notified
-
-        with patch("core.scripts.PushNotifications") as mock_notf:
-            self.script.process_loan(loan)
-            self.script.process_loan(loan2)
-
-        assert mock_notf.send_loan_expiry_message.call_count == 1
-        assert mock_notf.send_loan_expiry_message.call_args[0] == (
-            loan,
-            1,
-            [self.device_token],
-        )
-
-    def test_do_run(self, db: DatabaseTransactionFixture):
-        now = utc_now()
-        self._setup_method(db)
-        loan, _ = self.work.active_license_pool().loan_to(
-            self.patron,
-            now,
-            now + datetime.timedelta(days=1, hours=1),
-        )
-
-        work2 = db.work(with_license_pool=True)
-        loan2, _ = work2.active_license_pool().loan_to(
-            self.patron,
-            now,
-            now + datetime.timedelta(days=2, hours=1),
-        )
-
-        work3 = db.work(with_license_pool=True)
-        p = work3.active_license_pool()
-        loan3, _ = p.loan_to(
-            self.patron,
-            now,
-            now + datetime.timedelta(days=1, hours=1),
-        )
-        # loan 3 was notified today already, so should get skipped
-        loan3.patron_last_notified = now.date()
-
-        work4 = db.work(with_license_pool=True)
-        p = work4.active_license_pool()
-        loan4, _ = p.loan_to(
-            self.patron,
-            now,
-            now + datetime.timedelta(days=1, hours=1),
-        )
-        # loan 4 was notified yesterday, so should NOT get skipped
-        loan4.patron_last_notified = now.date() - datetime.timedelta(days=1)
-
-        self.script.process_loan = MagicMock()
-        self.script.BATCH_SIZE = 1
-        self.script.do_run()
-
-        assert self.script.process_loan.call_count == 3
-        assert self.script.process_loan.call_args_list == [
-            call(loan),
-            call(loan2),
-            call(loan4),
-        ]
-
-        # Sitewide notifications are turned off
-        self.script.process_loan.reset_mock()
-        ConfigurationSetting.sitewide(
-            db.session, Configuration.PUSH_NOTIFICATIONS_STATUS
-        ).value = ConfigurationConstants.FALSE
-        self.script.do_run()
-        assert self.script.process_loan.call_count == 0
 
 
 class TestWorkConsolidationScript:
