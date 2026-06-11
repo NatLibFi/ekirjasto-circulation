43d90c6ac6948c960c7b47da023f92d3c68dfd93
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2026-06-11 11:32:25 +0300

    EKIR-793: Refactor rwpm.py to import accessibility from dedicated module
    - Remove AccessMode, AccessModeSufficient, AccessibilityFeature, Hazard enums
    - Remove Accessibility, Certification, Exemption classes
    - Remove helper functions for enum coercion
    - Add imports from new api.opds.accessibility module
    - Significantly reduces rwpm.py file size and complexity

--- api/opds/rwpm.py
+++ api/opds/rwpm.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from collections.abc import Sequence
 from datetime import date, datetime
 from enum import Enum
@@ -15,10 +16,13 @@ from pydantic import (
     field_validator,
 )
 
+from api.opds.accessibility import Accessibility
 from api.opds.base import BaseOpdsModel
 from api.opds.types.language import LanguageCode, LanguageMap
 from api.opds.types.link import BaseLink, CompactCollection
 from api.opds.util import StrModelOrTuple, StrOrModel, StrOrTuple, obj_or_tuple_to_tuple
+
+_logger = logging.getLogger(__name__)
 
 
 class Encryption(BaseOpdsModel):
@@ -243,217 +247,6 @@ def _named_or_sequence_to_sequence(
         cls(name=item) if isinstance(item, str) else item  # type: ignore[misc]
         for item in obj_or_tuple_to_tuple(value)
     )
-
-
-class AccessMode(str, Enum):
-    """
-    https://github.com/readium/webpub-manifest/tree/master/contexts/default#accessmode-and-accessmodesufficient
-    """
-
-    auditory = "auditory"
-    visual = "visual"
-    textual = "textual"
-    chart_on_visual = "chartOnVisual"
-    chem_on_visual = "chemOnVisual"
-    color_dependent = "colorDependent"
-    diagram_on_visual = "diagramOnVisual"
-    math_on_visual = "mathOnVisual"
-    music_on_visual = "musicOnVisual"
-    text_on_visual = "textOnVisual"
-    tactile = "tactile"
-
-
-class AccessModeSufficient(str, Enum):
-    """
-    https://github.com/readium/webpub-manifest/tree/master/contexts/default#accessmode-and-accessmodesufficient
-    """
-
-    auditory = "auditory"
-    visual = "visual"
-    textual = "textual"
-    textual_visual = "textual, visual"
-    visual_textual = "visual, textual"
-    tactile = "tactile"
-
-
-class AccessibilityFeature(str, Enum):
-    """
-    https://github.com/readium/webpub-manifest/tree/master/contexts/default#features-and-hazards
-    """
-
-    structural_navigation = "structuralnavigation"
-    table_of_contents = "tableofcontents"
-    alternative_text = "alternativetext"
-    display_transformability = "displaytransformability"
-    reading_order = "readingorder"
-    is_fixed_layout = "isfixedlayout"
-    long_description = "longdescription"
-    long_descriptions = "longdescriptions"  # ?
-    described_math = "describedmath"
-    transcript = "transcript"
-    index_name = "index"
-    print_page_numbers = "printpagenumbers"
-    page_break_markers = "pagebreakmarkers"  # previously printPageNumbers https://www.w3.org/community/reports/a11y-discov-vocab/CG-FINAL-vocabulary-20241209/#pageBreakMarkers
-    audio_description = "audiodescription"
-    open_captions = "opencaptions"
-    page_break_source = "pagebreaksource"
-    page_navigation = "pagenavigation"
-    sign_language = "signlanguage"
-    synchronized_audio_text = "synchronizedaudiotext"
-    tactile_graphic = "tactilegraphic"
-    tactile_object = "tactileobject"
-    large_print = "largeprint"
-    high_contrast_audio = "highcontrastaudio"
-    high_contrast_display = "highcontrastdisplay"
-    braille = "braille"
-    closed_captions = "closedcaptions"
-    tts_markup = "ttsmarkup"
-    tagged_pdf = "taggedpdf"
-    math_ml = "mathml"
-    ruby_annotations = "rubyannotations"
-    full_ruby_annotations = "fullrubyannotations"
-    aria = "aria"
-    bookmarks = "bookmarks"
-    captions = "captions"
-    chem_ml = "chemml"
-    horizontal_writing = "horizontalwriting"
-    latex = "latex"
-    latex_chemistry = "latex-chemistry"
-    math_ml_chemistry = "mathml-chemistry"
-    timing_control = "timingcontrol"
-    unlocked = "unlocked"
-    vertical_writing = "verticalwriting"
-    with_additional_word_segmentation = "withadditionalwordsegmentation"
-    without_additional_word_segmentation = "withoutadditionalwordsegmentation"
-    none: str = "none"
-    unknown = "unknown"
-
-
-class Hazard(str, Enum):
-    """
-    https://github.com/readium/webpub-manifest/tree/master/contexts/default#features-and-hazards
-    """
-
-    flashing = "flashing"
-    no_flashing_hazard = "noflashinghazard"
-    unknown_flashing_hazard = "unknownflashinghazard"
-    motion_simulation = "motionsimulation"
-    no_motion_simulation_hazard = "nomotionsimulationhazard"
-    unknown_motion_simulation_hazard = "unknownmotionsimulationhazard"
-    sound = "sound"
-    no_sound_hazard = "nosoundhazard"
-    unknown_sound_hazard = "unknownsoundhazard"
-    none: str = "none"
-    unknown = "unknown"
-
-
-class Certification(BaseOpdsModel):
-    """
-    https://github.com/readium/webpub-manifest/tree/master/contexts/default#certification
-    """
-
-    certified_by: str | None = Field(None, alias="certifiedBy")
-    report: str | list[str] | None = None
-    credential: str | None = None
-
-
-class ConformsTo(str, Enum):
-    """
-    https://github.com/readium/webpub-manifest/tree/master/contexts/default#conformance
-    """
-
-    epub_1_0_wcag_2_0_level_a = (
-        "http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-a",
-    )
-    epub_1_0_wcag_2_0_level_aa = (
-        "http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-aa"
-    )
-    epub_1_0_wcag_2_0_level_aaa = (
-        "http://www.idpf.org/epub/a11y/accessibility-20170105.html#wcag-aaa"
-    )
-
-    epub_1_1_wcag_2_0_level_a = "https://www.w3.org/TR/epub-a11y-11#wcag-2.0-a"
-    epub_1_1_wcag_2_0_level_aa = "https://www.w3.org/TR/epub-a11y-11#wcag-2.0-aa"
-    epub_1_1_wcag_2_0_level_aaa = "https://www.w3.org/TR/epub-a11y-11#wcag-2.0-aaa"
-
-    epub_1_1_wcag_2_1_level_a = "https://www.w3.org/TR/epub-a11y-11#wcag-2.1-a"
-    epub_1_1_wcag_2_1_level_aa = "https://www.w3.org/TR/epub-a11y-11#wcag-2.1-aa"
-    epub_1_1_wcag_2_1_level_aaa = "https://www.w3.org/TR/epub-a11y-11#wcag-2.1-aaa"
-
-    epub_1_1_wcag_2_2_level_a = "https://www.w3.org/TR/epub-a11y-11#wcag-2.2-a"
-    epub_1_1_wcag_2_2_level_aa = "https://www.w3.org/TR/epub-a11y-11#wcag-2.2-aa"
-    epub_1_1_wcag_2_2_level_aaa = "https://www.w3.org/TR/epub-a11y-11#wcag-2.2-aaa"
-
-    epub_1_0_wcag_2_0_level_a_text = "EPUB Accessibility 1.0 - WCAG 2.0 Level A"
-    epub_1_0_wcag_2_0_level_aa_text = "EPUB Accessibility 1.0 - WCAG 2.0 Level AA"
-    epub_1_0_wcag_2_0_level_aaa_text = "EPUB Accessibility 1.0 - WCAG 2.0 Level AAA"
-
-    epub_1_0_wcag_2_1_level_a = "EPUB Accessibility 1.0 - WCAG 2.1 Level A"
-    epub_1_0_wcag_2_1_level_aa = "EPUB Accessibility 1.0 - WCAG 2.1 Level AA"
-    epub_1_0_wcag_2_1_level_aaa = "EPUB Accessibility 1.0 - WCAG 2.1 Level AAA"
-
-    epub_1_0_wcag_2_2_level_a = "EPUB Accessibility 1.0 - WCAG 2.2 Level A"
-    epub_1_0_wcag_2_2_level_aa = "EPUB Accessibility 1.0 - WCAG 2.2 Level AA"
-    epub_1_0_wcag_2_2_level_aaa = "EPUB Accessibility 1.0 - WCAG 2.2 Level AAA"
-
-    epub_1_1_wcag_2_0_level_a_text = "EPUB Accessibility 1.1 - WCAG 2.0 Level A"
-    epub_1_1_wcag_2_0_level_aa_text = "EPUB Accessibility 1.1 - WCAG 2.0 Level AA"
-    epub_1_1_wcag_2_0_level_aaa_text = "EPUB Accessibility 1.1 - WCAG 2.0 Level AAA"
-
-    epub_1_1_wcag_2_1_level_a_text = "EPUB Accessibility 1.1 - WCAG 2.1 Level A"
-    epub_1_1_wcag_2_1_level_aa_text = "EPUB Accessibility 1.1 - WCAG 2.1 Level AA"
-    epub_1_1_wcag_2_1_level_aaa_text = "EPUB Accessibility 1.1 - WCAG 2.1 Level AAA"
-
-    epub_1_1_wcag_2_2_level_a_text = "EPUB Accessibility 1.1 - WCAG 2.2 Level A"
-    epub_1_1_wcag_2_2_level_aa_text = "EPUB Accessibility 1.1 - WCAG 2.2 Level AA"
-    epub_1_1_wcag_2_2_level_aaa_text = "EPUB Accessibility 1.1 - WCAG 2.2 Level AAA"
-
-
-class Exemption(str, Enum):
-    """
-    https://github.com/readium/webpub-manifest/tree/master/contexts/default#exemption
-    """
-
-    eaa_disproportionate_burden = "eaa-disproportionate-burden"
-    eaa_fundamental_alteration = "eaa-fundamental-alteration"
-    eaa_microenterprise = "eaa-microenterprise"
-
-
-class Accessibility(BaseOpdsModel):
-    """
-    https://github.com/readium/webpub-manifest/tree/master/contexts/default#accessibility-metadata
-    """
-
-    feature: list[AccessibilityFeature] | None = None
-    access_mode: list[AccessMode] | None = Field(None, alias="accessMode")
-    access_mode_sufficient: list[AccessModeSufficient] | None = Field(
-        None, alias="accessModeSufficient"
-    )
-    hazard: list[Hazard] | None = None
-
-    # https://github.com/readium/webpub-manifest/tree/master/contexts/default#summary
-    summary: str | None = None
-    certification: Certification | None = None
-
-    # https://github.com/readium/webpub-manifest/tree/master/contexts/default#conformance
-    # Ellibs provides the data as a list, De Marque as a string. RWPM does not define either, it just says it can hold one or more profiles.
-    conforms_to: list[ConformsTo] | None = Field(None, alias="conformsTo")
-    exemption: Exemption | None = None
-
-    @field_validator("conforms_to", mode="before")
-    def validate_conforms_to(cls, value: str | list[str]) -> list[str]:
-        """Ensure that the conforms_to field is always a list of ConformsTo enums."""
-        if isinstance(value, str):
-            return [ConformsTo(value)]
-        elif isinstance(value, list):
-            return [ConformsTo(v) for v in value]
-
-    @field_validator("feature", "hazard", mode="before")
-    def to_lower(cls, values: list[str] | None) -> list[str] | None:
-        """Convert all feature strings to lowercase to ensure consistent mapping."""
-        if values is not None:
-            return [v.lower() for v in values]
-        return values
 
 
 class Metadata(BaseOpdsModel):
